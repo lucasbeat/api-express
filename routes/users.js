0acdb37f23c9a2c48464499c2c68cf521fbba116
lucas beat <clashbruno10@gmail.com>
2020-01-03 01:12:30 -0300

    adicionado criação de usuario

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const Users = require('../model/user')
+
 
 router.get('/users', (req, res) => {
-    return res.send({ message: 'tudo ok com o metodo GET' })
+    Users.find({}, (err, data) => {
+        if (err) return res.send({ error: `erro na consulta de usuarios` })
+        return res.send(data)
+    })
+})
+
+router.post('/create', (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) return res.send({ error: `dados insuficientes` })
+
+    Users.findOne({ email }, (err, data) => {
+        if (err) return res.send({ error: `erro ao buscar os usuarios` })
+        if (data) return res.send({ error: `usuario ja criado` })
+
+        Users.create(req.body, (err, data) => {
+            if (err) return res.send({ error: `erro ao criar o usuario` })
+
+            data.password = undefined
+            return res.send(data)
+        })
+    })
 })
 
 
