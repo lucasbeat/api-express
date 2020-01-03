b02f274b71b79cae5747bce3243944d4aba471cb
lucas beat <clashbruno10@gmail.com>
2020-01-03 00:32:49 -0300

    conectado ao mongo

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 
 const indexRoute = require('./routes/index.js')
 const usersRoute = require('./routes/users')
+
+//CONFIGURANDO O MONGOOSE
+var configMongoose = require('./config')
+const url = configMongoose
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+
+
+mongoose.connect(url, options)
+mongoose.set('useCreateIndex', true)
+
+mongoose.connection.on('connected', () => {
+    console.log('aplicação conectada ao banco de dados')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`erro no banco de dados: ${err}`)
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('aplicação desconectada')
+})
+
+//CONFIGURANDO O BODY-PARSER
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 app.use('/', indexRoute)
 app.use('/users', usersRoute)
