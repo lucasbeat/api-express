84046d2a54003b247aafbf09fdb7cd718c333340
lucas beat <clashbruno10@gmail.com>
2020-01-03 09:51:36 -0300

    adicionado criptografia ao campos

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Users = require('../model/user')
+const bcrypt = require('bcrypt')
 
 
 router.get('/users', (req, res) => {
@@ -28,5 +29,22 @@ router.post('/create', (req, res) => {
     })
 })
 
+router.post('/auth', (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) return res.send({ error: 'Dados insuficientes' })
+
+    Users.findOne({ email }, (err, data) => {
+        if (err) return res.send({ error: 'Erro ao buscar usuario' })
+        if (!data) return res.send({ error: 'usuario nao registrado' })
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if (!same) return res.send({ error: 'Erro ao atutenticar usuario' })
+
+            data.password = undefined
+            return res.send(data)
+        })
+    }).select('+password')
+})
 
 module.exports = router;
